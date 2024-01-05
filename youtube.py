b7544ea5c442143a71bce68338014a2d76272239
Tariq Elouzeh <tariq@Tariqs-Air.fios-router.home>
2024-01-04 23:55:25 -0500

    add logger and code refactor

--- youtube.py
+++ youtube.py
@@ -1,6 +1,7 @@
 import time
 import os
 from datetime import datetime
+import logging
 
 import secret
 
@@ -24,62 +25,67 @@ client = OpenAI(
     api_key=secret.chatgpt_api_key,
 )
 
-def log(msg):
-    formatted_timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"{formatted_timestamp} | INFO | {msg}\n")
+logging.basicConfig(filename='youtube.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+negative_comments = []
+negativity_check_counter = 0
 
 def get_my_videos():
-    uploads_playlist_request = yt.channels().list(
-        part='contentDetails',
-        id=secret.channel_id
-    )
+    logging.info("List my most recent 5 videos.")
 
-    uploads_playlist_response = uploads_playlist_request.execute()
-    uploads_playlist_id = uploads_playlist_response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    try:
+        response = yt.search().list(
+            part='snippet',
+            type='video',
+            order='date',
+            maxResults=5,
+            channelId=secret.channel_id
+        ).execute()
+    except Exception as e:
+        logging.error("Error listing the most recent videos: {}".format(e))
 
-    log("List my most recent 10 videos.")
-    playlist_items_request = yt.playlistItems().list(
-        part='contentDetails',
-        playlistId=uploads_playlist_id,
-        maxResults=10
-    )
+    videos = response.get('items', [])
 
-    videos = []
-
-    playlist_items_response = playlist_items_request.execute()
-
-    for item in playlist_items_response['items']:
-        videos.append(item['contentDetails']['videoId'])
-
-    log(videos)
-    time.sleep(3)
+    videos_list = []
+    for video in videos:
+        video_id = video['id']['videoId']
+        video_title = video['snippet']['title']
+        videos_list.append([video_id, video_title])
     
-    return videos
+    logging.info(videos_list)
+    time.sleep(3)
+    return videos_list
 
-def check_latest_comments(video_id):
-    log(f"List the most recent 50 comments for video {video_id}")
+def check_latest_comments(video_item):
+    global negative_comments
+    logging.info(f"List the most recent 100 comments for video {video_item[0]}")
     time.sleep(2)
     comments_request = yt.commentThreads().list(
         part='snippet',
-        videoId=video_id,
-        order='time',  # Order by time to get the latest comments first
-        maxResults=50   # Adjust as needed
+        videoId=video_item[0],
+        order='time',
+        maxResults=100
     )
 
-    comments_response = comments_request.execute()
+    try:
+        comments_response = comments_request.execute()
+    except Exception as e:
+        logging.error("Error listing the latest comments: {}".format(e))
 
     i = 1
     for item in comments_response['items']:
         comment_id = item['id']
         comment_snippet = item['snippet']['topLevelComment']['snippet']
         text = comment_snippet['textDisplay']
-        log(f"Comment {i}: comment id: {comment_id} | {text}")
-        log("Checking if the comment is negative...")
+        logging.info(f"Comment {i}: comment id: {comment_id} | {text}")
+        logging.info("Checking if the comment is negative...")
         if is_negative(text):
-            log("Comment is negative. Deleting it...")
-            delete_comment(comment_id)
+            negative_comments.append([video_item[1], text])
+            logging.warning("Comment is negative. Deleting it...")
+            # TODO uncomment the following line if you are sure of deleting the negative comments
+            #delete_comment(comment_id)
         else:
-            log("Comment is ok, moving on.")
+            logging.info("Comment is ok, moving on.")
         i+=1
 
 def delete_comment(comment_id):
@@ -87,30 +93,41 @@ def delete_comment(comment_id):
 
     try:
         yt.comments().delete(id=comment_id).execute()
-        print("Comment deleted successfully.")
+        logging.info("Comment deleted successfully.")
     except Exception as e:
-        log("Error deleting comment: {}".format(e))
+        logging.error("Error deleting comment: {}".format(e))
 
 def is_negative(comment):
+    global negativity_check_counter
     prompt = "Does this sentence have negative, aggressive, hateful or discouraging language? Say yes or no: \n"
     prompt += comment
     response = client.chat.completions.create(
         model="gpt-4",
         messages=[{"role": "user", "content": prompt}]
     )
+    negativity_check_counter = negativity_check_counter + 1
     return response.choices[0].message.content.lower().strip() == "yes"
+
+def generate_report():
+    logging.info(f"\n\n--------------REPORT--------------")
+    logging.info(f"Checked {negativity_check_counter} comment and found {len(negative_comments)} negative comments.\n")
+    for comment in negative_comments:
+        logging.info(f"Video: {comment[0]} | Comment: {comment[1]}")
+    logging.info(f"\n\n--------------END REPORT--------------")
 
 def main():
     """
     1- Get my recent videos
     2- List all comments for a video (include comment text and id)
-    3- For each comment, check positivity using chatgpt (return Positive, Negative or Neutral)
+    3- For each comment, check positivity using chatgpt (return yes or no)
     4- delete any negative comment using comment id.
     """
     time.sleep(3)
     videos = get_my_videos()
-    for video_id in videos:
-        check_latest_comments(video_id)
+    for video_item in videos:
+        check_latest_comments(video_item)
+    
+    generate_report()
 
 
 if __name__ == "__main__":
